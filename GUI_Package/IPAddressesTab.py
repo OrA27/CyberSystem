962365de5aaa162bed25364cfb36d56b38f65b64
Or Alush <Or.Alush@solaredge.com>
2023-07-24 19:47:45 +0300

    BIG UPDATE: added support to macOS by changing UI to PyQt6 recreated main GUI window from scratch to a more simplistic look
    icons are still WIP

--- GUI_Package/IPAddressesTab.py
+++ GUI_Package/IPAddressesTab.py
@@ -1,7 +1,7 @@
 from . import *
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QFont, QTransform, QIcon
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QHBoxLayout, QStyle
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QFont, QTransform, QIcon
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QLineEdit, QHBoxLayout, QStyle
 
 
 # TODO 30/04/2023 Amit: Connect 'enter' keypress to V button.
@@ -19,23 +19,23 @@ class IPAddressTab(QWidget):
         self.setLayout(self.layout)
 
         self.label = QLabel("Your addresses:")  # ip tab label
-        self.layout.addWidget(self.label) # add ip tab lable to tabs layout
+        self.layout.addWidget(self.label)  # add ip tab lable to tabs layout
 
         # add existing ip addresses
         self.existing_ips = VerticalBox()
         self.layout.addWidget(self.existing_ips)
 
-        self.new_ip_widget = QWidget() # new ip widget to contain new ip layout
-        self.new_ip_layout = QHBoxLayout(self.new_ip_widget) # new ip layout to contain new ip text field and button
-        self.new_ip_text_field = QLineEdit() # creates new text field for new ip
+        self.new_ip_widget = QWidget()  # new ip widget to contain new ip layout
+        self.new_ip_layout = QHBoxLayout(self.new_ip_widget)  # new ip layout to contain new ip text field and button
+        self.new_ip_text_field = QLineEdit()  # creates new text field for new ip
         self.new_ip_text_field.setFixedWidth(200)
 
         # creates button to add and validate the new ip
         self.new_ip_button = QPushButton()
-        icon_str = 'SP_DialogApplyButton'
-        pixmapi = getattr(QStyle, icon_str)
-        add_icon = self.style().standardIcon(pixmapi)
-        self.new_ip_button.setIcon(add_icon)
+        # icon_str = 'SP_DialogApplyButton'
+        # pixmapi = getattr(QStyle, icon_str)
+        # add_icon = self.style().standardIcon(pixmapi)
+        self.new_ip_button.setIcon(QIcon("SP_DialogApplyButton"))
         self.new_ip_button.setFixedSize(50, 50)
         self.new_ip_button.setStyleSheet("border-radius: 25px; background-color: #f2f2f2;")
         self.new_ip_button.clicked.connect(lambda: add_new_ip())
@@ -43,19 +43,20 @@ class IPAddressTab(QWidget):
         # add new ip elements to the layout
         self.new_ip_layout.addWidget(self.new_ip_text_field)
         self.new_ip_layout.addWidget(self.new_ip_button)
-        self.new_ip_layout.setAlignment(Qt.AlignLeft)
+        self.new_ip_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
 
-        self.new_ip_widget.setLayout(self.new_ip_layout) # add the new ip layout to new ip widget
-        self.new_ip_widget.hide() # hide the new ip widget
-        self.layout.addWidget(self.new_ip_widget) # add new ip widget to ip tab layout
-        self.setFont(QFont("Ariel", 14)) # set tabs font
-        self.layout.setAlignment(Qt.AlignTop) # set tabs allignment
+        self.new_ip_widget.setLayout(self.new_ip_layout)  # add the new ip layout to new ip widget
+        self.new_ip_widget.hide()  # hide the new ip widget
+        self.layout.addWidget(self.new_ip_widget)  # add new ip widget to ip tab layout
+        self.setFont(QFont("Ariel", 14))  # set tabs font
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignTop)  # set tabs alignment
 
         # Add a circular button to the tab
         self.button = QPushButton()
-        icon_str = 'SP_TitleBarCloseButton'
-        pixmapi = getattr(QStyle, icon_str)
-        plus_icon = self.style().standardIcon(pixmapi)
+        # icon_str = 'SP_TitleBarCloseButton'
+        # pixmapi = getattr(QStyle, icon_str)
+        # plus_icon = self.style().standardIcon(pixmapi)
+        plus_icon = QIcon("SP_TitleBarCloseButton")
         transform = QTransform().rotate(45)
         rotated_pixmap = plus_icon.pixmap(64, 64).transformed(transform)
         plus_icon = QIcon(rotated_pixmap)
@@ -77,10 +78,9 @@ class IPAddressTab(QWidget):
             new_ip_to_add = HorizontalBox("IP", self.new_ip_text_field.text())
             self.existing_ips.add_row(new_ip_to_add)
 
-            #TODO use find open ports function and add ip and ports to the list in common variables as a dict
+            # TODO use find open ports function and add ip and ports to the list in common variables as a dict
 
             self.new_ip_text_field.clear()
 
         def validate_ip(ip):
             pass
-
--- GUI_Package/__init__.py
+++ GUI_Package/__init__.py
@@ -1,9 +1,8 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import Qt
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import Qt
 import pkgutil
 
 
-# classes
 class HorizontalBox(QWidget):
     def __init__(self, box_type, label_text):
         super().__init__()
@@ -32,27 +31,26 @@ class HorizontalBox(QWidget):
             self.label.setToolTip(self.label.text())
             # add widgets to layout
             self.layout.addWidget(self.label)
-            self.layout.addWidget(self.button, alignment=Qt.AlignLeft)
+            self.layout.addWidget(self.button, alignment=Qt.AlignmentFlag.AlignLeft)
 
-        elif box_type == "script":
+        elif box_type == 'script':
             self.button = QCheckBox()  # create the button
             # add widgets to layout
-            self.layout.addWidget(self.button, alignment=Qt.AlignLeft)
-            self.layout.addWidget(self.label, alignment=Qt.AlignLeft)
-            self.layout.setAlignment(Qt.AlignLeft)
+            self.layout.addWidget(self.button, alignment=Qt.AlignmentFlag.AlignLeft)
+            self.layout.addWidget(self.label, alignment=Qt.AlignmentFlag.AlignLeft)
+            self.layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
             self.layout.setContentsMargins(0, 10, 0, 10)
 
         else:
             raise Exception("box type invalid")
 
-
     def remove(self):
         if self.box_type == "IP":
             parent_layout = self.parent()
             parent_layout.layout.removeWidget(self)
-            #TODO delete ip from common variables
+            # TODO delete ip from common variables
         else:
-            raise Exception("Function and ox type mismatch")
+            raise Exception("Function and box type mismatch")
 
 
 class VerticalBox(QWidget):
@@ -60,14 +58,13 @@ class VerticalBox(QWidget):
         super().__init__()
 
         self.layout = QVBoxLayout(self)
-        self.layout.setAlignment(Qt.AlignTop)
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         self.setLayout(self.layout)
 
     def add_row(self, row: QWidget):
         self.layout.addWidget(row)
 
 
-# functions
 def list_package_modules(package_name):
     """
     List all the modules of a given package, ignoring any submodules.
