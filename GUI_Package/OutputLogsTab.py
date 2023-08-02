9dab8ec0e4190cbb960a814d41ba300fb21ef66a
Or Alush <Or.Alush@solaredge.com>
2023-08-02 18:11:20 +0300

    added code line for time stamp

--- GUI_Package/OutputLogsTab.py
+++ GUI_Package/OutputLogsTab.py
@@ -1,9 +1,15 @@
 from PyQt6.QtWidgets import QWidget, QVBoxLayout, QLabel, QTextEdit
+import datetime
+
+
+# TODO: create new log files each run,
+#  naming convention will be according to current time
 
 
 class OutputLogsTab(QWidget):
     def __init__(self):
         super().__init__()
+        # current_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S") # get time stamp of current
 
         # Create a layout for the tab
         self.layout = QVBoxLayout(self)
