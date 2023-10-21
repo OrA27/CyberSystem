f749bec84229be4c42ce8f793b816afb85f767ee
Or Alush <Or.Alush@solaredge.com>
2023-10-21 21:28:38 +0300

    small fixes on plots

--- GUI_Package/OutputTab.py
+++ GUI_Package/OutputTab.py
@@ -63,6 +63,6 @@ class OutputTab(QWidget):
 
 class Canvas(FigureCanvasQTAgg):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(Canvas, self).__init__(fig)
+        self.fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = self.fig.add_subplot(111)
+        super(Canvas, self).__init__(self.fig)
