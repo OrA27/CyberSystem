aabf5108546841f16ccec1acc99eddcc7df0c212
AmitHayoun <amithayoun0@gmail.com>
2023-11-24 21:28:59 +0200

    fix bug in data interception

--- Cyber_Scripts/Data Interception.py
+++ Cyber_Scripts/Data Interception.py
@@ -8,6 +8,8 @@ import threading
 from Cyber_Scripts import *
 from PyQt6.QtWidgets import QTextEdit
 
+global number_of_packets
+
 """
 def get_internal_ipv4():
     try:
@@ -42,21 +44,31 @@ def get_ip_address(url):
         return None
 
 def packet_filter(packet, your_ip, server_ip):
-    return (
+    # print(packet.summary())
+    global number_of_packets
+    # print(number_of_packets)
+    if (
         IP in packet and
         packet[IP].src == your_ip and
         packet[IP].dst == server_ip and
-        TCP in packet and
-        Raw in packet and
-        b'POST' in packet["Raw"].load  # Check if "POST" is in the packet content
-    )
+        TCP in packet):
+            number_of_packets += 1
+            return(
+            Raw in packet and
+            b'POST' in packet["Raw"].load  # Check if "POST" is in the packet content
+            or
+            number_of_packets>30
+            )
+
 
 
 def execute(login_page_url):
+    global number_of_packets
+    number_of_packets = 0
     try:
-        domain = login_page_url.split("/")[2]
         values_found = 0
 
+        # used in non-local networks
         """
         response = None
         while not response:
@@ -82,21 +94,25 @@ def execute(login_page_url):
         send_data_thread = threading.Thread(target=enter_login_input, args=[login_page_url, "user name", "password"])
         send_data_thread.start()
         post_packet = sniff(filter=f"tcp", lfilter=lambda pkt: packet_filter(pkt, my_ip, login_page_ip), count=1)
-
-        packet_content = post_packet[0]["Raw"].load.decode()
-        packet_parts = packet_content.split("\r\n")
-        packet_variables_part = packet_parts[-1]
-        text_variables = packet_variables_part.split("&")
-        for text_variable in text_variables:
-            variable_vals = text_variable.split("=")
-            if len(variable_vals) > 1:
-                if variable_vals[1] in ["user+name", "password"]:
-                    values_found += 1
-            else:
-                break
-        return values_found == 2
-    except:
+        if post_packet[0].getlayer("Raw"):
+            packet_content = post_packet[0]["Raw"].load.decode()
+            packet_parts = packet_content.split("\r\n")
+            packet_variables_part = packet_parts[-1]
+            text_variables = packet_variables_part.split("&")
+            for text_variable in text_variables:
+                variable_vals = text_variable.split("=")
+                if len(variable_vals) > 1:
+                    if variable_vals[1] in ["user+name", "password"]:
+                        values_found += 1
+                else:
+                    break
+            return values_found == 2
+        else:
+            return False
+    except Exception as e:
+        print(e)
         return None
 
 
-# execute(login_page_url='http://seatassist.byethost32.com/pages/index.php')
+# print(execute(login_page_url='https://test.local/site/login.php'))
+# print(execute(login_page_url='http://test.local/site/login.php'))
